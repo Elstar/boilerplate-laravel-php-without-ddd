b91282be511b8d9fe8ecdd13483b92d270f39343
Sergii Dembitskyi <sergiy.dmb@gmail.com>
2023-12-26 10:50:39 +0200

    first commit

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Relationship\PermissionRelationship;
+use App\Models\Traits\Scope\PermissionScope;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+/**
+ * Class Permission.
+ */
+class Permission extends SpatiePermission
+{
+    use PermissionRelationship,
+        PermissionScope;
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Attribute\RoleAttribute;
+use App\Models\Traits\Method\RoleMethod;
+use App\Models\Traits\Scope\RoleScope;
+use Database\Factories\RoleFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role as SpatieRole;
+
+/**
+ * Class Role.
+ */
+class Role extends SpatieRole
+{
+    use HasFactory,
+        RoleAttribute,
+        RoleMethod,
+        RoleScope;
+
+    /**
+     * @var string[]
+     */
+    protected $with = [
+        'permissions',
+    ];
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return RoleFactory::new();
+    }
+}
